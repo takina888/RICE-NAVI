52f9122f134549ad3cbbb7e5e31ef14bb10b14aa
takina888 <takinaito@gmail.com>
2026-06-16 15:55:20 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rice-navi-trouble-v1';
+const CACHE_NAME = 'rice-navi-trouble-v1-2';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./data.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
