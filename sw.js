9c300f8335f7f59d437fe36454ffc7ea5c377d0f
takina888 <takinaito@gmail.com>
2026-06-18 09:13:44 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='rice-navi-v13-source-rebuild';self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/rn-icon.png'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
